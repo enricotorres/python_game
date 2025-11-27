878a1f20e4d1ff54fe9daaeee50b8f693133e0a3
Enrico Torres <enricotorres@MacBook-Air-de-Enrico.local>
2025-11-27 17:03:05 -0300

    move BattleScene to scenarios.py

--- controllers.py
+++ controllers.py
@@ -1,5 +1,6 @@
 #from graphics import *
 from classes import Pokemon, Trainer, Move
+from scenarios import BattleScene
 import random
 import json
 
@@ -191,26 +192,6 @@ class BattleController:
         enemy.add_pokemon(charmander)
 
         return player, enemy
-
-#funcao teste
-class BattleScene:
-    def __init__(self, scene):
-        self.scene = scene
-
-    def chose_action(self):
-        return input("escolha a acao  ")
-
-    def chose_pokemon(self):
-        return int(input("escolha o pokemon por indice "))
-
-    def chose_item(self):
-        return int(input("escolha o item por indice "))
-
-    def chose_attack(self):
-        return int(input("escolha o ataque por indice "))
-
-    def run(self):
-        return
 
 battle_scene = BattleScene("scene")
 
--- world_manager.py
+++ world_manager.py
@@ -1,9 +1,12 @@
 from graphics import *
 from scenarios import BattleScene
 
+
+battle_scene = BattleScene
+
 class WorldManager:
-    def __init__(self, BattleScene):
-        self.battle_scene = BattleScene
+    def __init__(self, battle_scene):
+        self.battle_scene = battle_scene
 
 
 
