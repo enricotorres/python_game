9b59fc89d0fcd472649299a1e7fe39bfdcbd2659
Enrico Torres <enricotorres@MacBook-Air-de-Enrico.local>
2025-11-27 16:58:25 -0300

    create WorldManager class

---
+++ world_manager.py
@@ -0,0 +1,11 @@
+from graphics import *
+from scenarios import BattleScene
+
+class WorldManager:
+    def __init__(self, BattleScene):
+        self.battle_scene = BattleScene
+
+
+
+    def change_scene(self, scene):
+        pass
